fc9034da11c9354582f5b11de610b72962c046ef
Tom <tom@tjh.local>
2017-12-07 12:55:59 +0000

    Initial Commit

---
+++ SSIDChecker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SSIDChecker
+//
+//  Created by Thomas Holbrook on 07/12/2017.
+//  Copyright © 2017 Thomas Holbrook. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
